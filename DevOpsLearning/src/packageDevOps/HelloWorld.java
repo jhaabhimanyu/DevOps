45f5078c5f29f402e7672dc24ee7168b19b41de6
abhim <abhim@DESKTOP-FVDGD53>
2021-02-24 00:25:35 +0530

    Commit 2

--- DevOpsLearning/src/packageDevOps/HelloWorld.java
+++ DevOpsLearning/src/packageDevOps/HelloWorld.java
@@ -3,7 +3,9 @@ package packageDevOps;
 public class HelloWorld {
 
 	public static void main(String[] args) {
+		
 		System.out.println("Learning DevOps");
+		System.out.println("Hello World" );
 
 	}
 
